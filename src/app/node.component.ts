86afa0d8772e1cf7b7f999e30b10442aea2a35a5
vlasov.s <vlasov.s@orionnet.ru>
2018-07-25 17:48:51 +0700

    try to set style

--- src/app/node.component.ts
+++ src/app/node.component.ts
@@ -2,14 +2,12 @@ import {Component, Input} from "@angular/core";
 
 @Component({
   selector: 'tree-node',
-  template: `
-    <div>
-        <div class="line-top"></div>
+  template: `    
+        <div class="line-to"></div>
         <div  myDropTarget (myDrop)="onDrop($event)" [myDraggable]="{data: nodeTree.name}" class="draggable center rect">{{nodeTree.name}}</div>
-        <div *ngIf="nodeTree.children.length" class="center flex-container">
+        <div *ngIf="nodeTree.children.length" class="center flex-container ">
           <tree-node *ngFor="let ch of nodeTree.children" [nodeTree]="ch" ></tree-node>
         </div>
-    </div>
   `,
   styles: [
     `      
@@ -23,23 +21,34 @@ import {Component, Input} from "@angular/core";
         font-size: 15px;
         border-radius: 2px;
         margin: 0 auto;
-        margin-top:30px;
-        width: 6rem;
+        margin-top:60px;
       }
       .rect::before {
         content: "";
         width: calc(50% - 1px);
-        height: 30px;
-        left: 0;
+        position: relative;
+        height: 100px;
+        left: calc(50%);
         top: -30px;
         border-right: 1px solid #dadada;
       }
+
+      /*.flex-container::before {*/
+        /*content: "";*/
+        /*width: calc(50% - 1px);*/
+        /*position: relative;*/
+        /*height: 30px;*/
+        /*top: 30px;*/
+        /*left: calc(50%);*/
+        /*border-right: 1px solid #dadada;*/
+      /*}*/
       .center {
         justify-content: center;
       }
     .draggable {
       border: 1px solid #ccc;
       padding: 1rem;
+      width: 6rem;
       cursor: move;
     }
     `],
